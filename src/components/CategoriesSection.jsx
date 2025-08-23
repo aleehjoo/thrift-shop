e0c72cb6263e282781bc8df2b886a9c3ebad42de
aleehjoo <alejandroumila2@gmail.com>
2025-08-23 23:54:08 +0800

    Adjusting values per card rendered in categories section

--- src/components/CategoriesSection.jsx
+++ src/components/CategoriesSection.jsx
@@ -3,7 +3,10 @@ import CategoryCard from "./CategoryCard.jsx";
 
 const CategoriesSection = () => {
     return (
-        <div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-16 justify-items-center">
+            <CategoryCard />
+            <CategoryCard />
+            <CategoryCard />
             <CategoryCard />
         </div>
     )
--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const CategoryCard = () => {
     return (
-        <div>CategoryCard</div>
+        <div className="flex flex-col bg-neutral-700 w-70 h-90 text-white rounded-2xl">
+            <div className="w-full h-[80%]">
+
+            </div>
+            <div className="mt-auto w-full h-[20%]">
+
+            </div>
+        </div>
     )
 }
 export default CategoryCard
